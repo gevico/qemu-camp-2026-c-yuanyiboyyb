d95599db39c2396af5d5511185d3332b0cacaba1
yuanyiboyyb <yuanyiboyyb@outlook.com>
2026-05-17 23:42:31 +0800

    update

--- exercises/20_mybash/src/myfile/myfile.c
+++ exercises/20_mybash/src/myfile/myfile.c
@@ -30,11 +30,15 @@ void print_elf_type(uint16_t e_type) {
 }
 
 int __cmd_myfile(const char* filename) {
-    char filepath[256];
+    char filepath[512];
     int fd;
     Elf64_Ehdr ehdr;
 
-    strcpy(filepath, filename);
+    strncpy(filepath, filename, sizeof(filepath) - 1);
+    filepath[sizeof(filepath) - 1] = '\0';
+    if (strncmp(filepath, "/workspace/", 11) == 0) {
+      snprintf(filepath, sizeof(filepath), "/home/yyb/qemu-camp-2026-c-yuanyiboyyb/%s", filename + 11);
+    }
     fflush(stdout);
     printf("filepath: %s\n", filepath);
 
--- exercises/20_mybash/src/mywc/mywc.c
+++ exercises/20_mybash/src/mywc/mywc.c
@@ -111,6 +111,12 @@ void process_file(const char *filename) {
 }
 
 int __cmd_mywc(const char* filename) {
-  process_file(filename);
+  char filepath[512];
+  strncpy(filepath, filename, sizeof(filepath) - 1);
+  filepath[sizeof(filepath) - 1] = '\0';
+  if (strncmp(filepath, "/workspace/", 11) == 0) {
+    snprintf(filepath, sizeof(filepath), "/home/yyb/qemu-camp-2026-c-yuanyiboyyb/%s", filename + 11);
+  }
+  process_file(filepath);
   return 0;
 }
